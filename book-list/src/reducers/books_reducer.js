e578f853537091c14eec652e3edea295fd4497e7
yohannli2015 <yohannli2015@gmail.com>
2017-08-27 15:17:19 -0400

    update ReactJS

---
+++ book-list/src/reducers/books_reducer.js
@@ -0,0 +1,8 @@
+export default function() {
+	return [
+		{title:'JS: The Weired Parts', pages:401},
+		{title:'Harry Potter', pages:239},
+		{title:'The Dark Knight', pages:288},
+		{title:'RUBY Gaming', pages:94}
+	]
+}
---
+++ book-list/src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import BooksReducer from './books_reducer';
+import ActiveBookReducer from './active-book_reducer';
+
+const rootReducer = combineReducers({
+  books:BooksReducer,
+  activeBook:ActiveBookReducer
+});
+
+export default rootReducer;
